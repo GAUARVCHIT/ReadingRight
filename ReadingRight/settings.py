857830bf3ff85516e6d69feb7728c082461b05b3
GAURAVCHIT <gaurav198.rr@gmail.com>
2021-01-31 12:29:56 +0530

    swagger, restframework, and simplejwt support added

--- ReadingRight/settings.py
+++ ReadingRight/settings.py
@@ -37,7 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
+    'authentication.apps.AuthenticationConfig',
 ]
+
+REST_FRAMEWORK={
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
